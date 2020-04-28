849d0c9c058324742de58b55e3ff6336233d5287
Alexander V <avorozhtsov@gmail.com>
2020-04-28 16:59:07 +0300

    initial commit

---
+++ TestWpfApplication/CustomControlViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TestWpfApplication
+{
+  public class CustomProperty : INotifyPropertyChanged
+  {
+    private string nameSelected;
+    private string valueSelected;
+
+    public string Name { get; set; } = new Random().Next().ToString();
+
+    public string NameSelected
+    {
+      get => nameSelected;
+      set
+      {
+        nameSelected = value;
+      }
+    }
+
+    public string Value { get; set; } = new Random().Next().ToString();
+
+    public string ValueSelected
+    {
+      get => valueSelected;
+      set
+      {
+        valueSelected = value;
+      }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+  }
+
+  public class CustomControlViewModel
+  {
+    private ObservableCollection<CustomProperty> customProperties = new ObservableCollection<CustomProperty>(
+      new[]{
+        new CustomProperty(),
+        new CustomProperty(),
+        new CustomProperty(),
+      });
+
+    public ObservableCollection<CustomProperty> CustomProperties
+    {
+      get => customProperties;
+      set => customProperties = value;
+    }
+
+    private Timer timer;
+
+    public CustomControlViewModel()
+    {
+      timer = new Timer(state =>
+      {
+        Application.Current.Dispatcher.Invoke(UpdateData);
+      }, null, 0, 1000);
+    }
+
+    private void UpdateData()
+    {
+      //  To stop
+      int i = 3;
+
+      //CustomProperties.Clear();
+      //foreach (var customProperty in Enumerable.Range(0, new Random().Next(10))
+      //  .Select(x => new CustomProperty()))
+      //{
+      //  CustomProperties.Add(customProperty);
+      //}
+    }
+  }
+}
---
+++ TestWpfApplication/SelectableTextBox.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWpfApplication
+{
+  /// <summary>
+  /// Interaction logic for SelectableTextBox.xaml
+  /// </summary>
+  public partial class SelectableTextBox : TextBox
+  {
+    public SelectableTextBox()
+    {
+      InitializeComponent();
+    }
+
+    public string ReadonlySelectionText
+    {
+      get
+      {
+        return (string)GetValue(ReadonlySelectionTextProperty);
+      }
+      set
+      {
+        var oldValue = ReadonlySelectionTextProperty;
+        SetValue(ReadonlySelectionTextProperty, value);
+      }
+    }
+
+    public static readonly DependencyProperty ReadonlySelectionTextProperty =
+      DependencyProperty.Register(nameof(ReadonlySelectionText),
+        typeof(string),
+        typeof(SelectableTextBox),
+        new FrameworkPropertyMetadata(string.Empty));
+    
+    protected override void OnSelectionChanged(RoutedEventArgs e)
+    {
+      ReadonlySelectionText = SelectedText;
+      base.OnSelectionChanged(e);
+    }
+  }
+}
